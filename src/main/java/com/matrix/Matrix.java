0b830d997bb39804927a30447780f3129e5cc284
piotter121 <defcon1994@gmail.com>
2015-12-09 20:55:29 +0100

    Dodanie klasy testów

--- src/main/java/com/matrix/Matrix.java
+++ src/main/java/com/matrix/Matrix.java
@@ -5,39 +5,22 @@ package com.matrix;
  */
 public class Matrix implements MatrixOperations {
 
-    private double[][] matrix;
-
-    public Matrix(int x , int y)
-    {
-        matrix = new double [x][y];
-    }
-
-    public double getValue(int x , int y)
-    {
-        return matrix[x][y];
-    }
-
-    @Override
     public Matrix add(Matrix m) {
         return null;
     }
 
-    @Override
     public Matrix subtract(Matrix m) {
         return null;
     }
 
-    @Override
     public Matrix multiply(Matrix m) {
         return null;
     }
 
-    @Override
     public Matrix inversion() {
         return null;
     }
 
-    @Override
     public Matrix transposition() {
         return null;
     }
---
+++ src/main/java/com/matrix/MatrixTest.java
@@ -0,0 +1,43 @@
+package com.matrix;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * MatrixTDD
+ * Created by Piotrek on 09-12-2015.
+ */
+public class MatrixTest {
+
+    @Before
+    public void setUp() throws Exception {
+
+    }
+
+    @Test
+    public void testAdd() throws Exception {
+
+    }
+
+    @Test
+    public void testSubtract() throws Exception {
+
+    }
+
+    @Test
+    public void testMultiply() throws Exception {
+
+    }
+
+    @Test
+    public void testInversion() throws Exception {
+
+    }
+
+    @Test
+    public void testTransposition() throws Exception {
+
+    }
+}
