02e3063ad535ff0bd41f3d3343d028e157ae0ff2
piotter121 <defcon1994@gmail.com>
2015-12-09 21:33:49 +0100

    Dodanie macierzy jednostkowej, zerowej i kwadratowej do testów

--- src/main/java/com/matrix/MatrixTest.java
+++ src/main/java/com/matrix/MatrixTest.java
@@ -10,10 +10,32 @@ import static org.junit.Assert.*;
  * Created by Piotrek on 09-12-2015.
  */
 public class MatrixTest {
+    private Matrix jednostkowa;
+    private Matrix zerowa;
+    private Matrix zwyklaKwadratowa;
 
     @Before
     public void setUp() throws Exception {
-
+        jednostkowa = new Matrix(3,3);
+        zerowa = new Matrix(3,3);
+        zwyklaKwadratowa = new Matrix(3,3);
+        for (int i = 0; i < 3; i++)
+            for (int j = 0; j < 3; j++) {
+                if (i == j)
+                    jednostkowa.set(i,j,1);
+                else
+                    jednostkowa.set(i,j,0);
+                zerowa.set(i,j,0);
+            }
+        zwyklaKwadratowa.set(0,0,6);
+        zwyklaKwadratowa.set(0,1,3);
+        zwyklaKwadratowa.set(0,2,6);
+        zwyklaKwadratowa.set(1,0,2);
+        zwyklaKwadratowa.set(1,1,3);
+        zwyklaKwadratowa.set(1,2,5);
+        zwyklaKwadratowa.set(2,0,-1);
+        zwyklaKwadratowa.set(2,1,2);
+        zwyklaKwadratowa.set(2,2,4);
     }
 
     @Test
