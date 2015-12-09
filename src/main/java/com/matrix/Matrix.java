1e4629f7a6aab77858e9240b2e555a6a0df08dab
piotter121 <defcon1994@gmail.com>
2015-12-09 21:17:43 +0100

    Dodanie konstruktora

--- src/main/java/com/matrix/Matrix.java
+++ src/main/java/com/matrix/Matrix.java
@@ -4,6 +4,15 @@ package com.matrix;
  * Created by Matexo on 2015-12-09.
  */
 public class Matrix implements MatrixOperations {
+    private double[][] data;
+    private int rows;
+    private int cols;
+
+    public Matrix(int rows, int cols) {
+        this.rows = rows;
+        this.cols = cols;
+        data = new double[rows][cols];
+    }
 
     public double get(int i, int j) {
         return 0;
